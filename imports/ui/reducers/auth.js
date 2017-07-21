dd091549b24ff76f4ac1ac3e009b5f07aa90cc1f
niinyarko <niinyarko1@gmail.com>
2017-07-21 08:51:16 +0000

    add currentUser to state.

--- imports/ui/reducers/auth.js
+++ imports/ui/reducers/auth.js
@@ -5,12 +5,22 @@ import {
 } from '../actions/types';
 
 
-const INITIAL_STATE = { loggedIn: !!Meteor.userId(), loggingIn: false, error: 0 };
+const INITIAL_STATE = {
+  loggedIn: !!Meteor.userId(),
+  loggingIn: false,
+  error: 0,
+  currentUser: Meteor.userId(),
+};
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case SET_AUTH:
-      return { ...state, loggedIn: action.newState, error: action.error };
+      return {
+        ...state,
+        loggedIn: action.newState,
+        error: action.error,
+        currentUser: Meteor.userId(),
+      };
     case LOGIN_IN:
       return { ...state, loggingIn: action.loggingIn };
     default:
